668ec58c96e889d2574d984ab8da8ac59c46ce0d
Scott Windsor <swindsor@amazon.com>
2017-02-24 00:40:39 -0800

    Vendoring latest docker-credential-helpers

--- ecr-login/vendor/github.com/docker/docker-credential-helpers/credentials/helper.go
+++ ecr-login/vendor/github.com/docker/docker-credential-helpers/credentials/helper.go
@@ -1,14 +1,5 @@
 package credentials
 
-import "errors"
-
-// Credentials holds the information shared between docker and the credentials store.
-type Credentials struct {
-	ServerURL string
-	Username  string
-	Secret    string
-}
-
 // Helper is the interface a credentials store helper must implement.
 type Helper interface {
 	// Add appends credentials to the store.
@@ -18,8 +9,6 @@ type Helper interface {
 	// Get retrieves credentials from the store.
 	// It returns username and secret as strings.
 	Get(serverURL string) (string, string, error)
+	// List returns the stored serverURLs and their associated usernames.
+	List() (map[string]string, error)
 }
-
-// ErrCredentialsNotFound standarizes the not found error, so every helper returns
-// the same message and docker can handle it properly.
-var ErrCredentialsNotFound = errors.New("credentials not found in native keychain")
